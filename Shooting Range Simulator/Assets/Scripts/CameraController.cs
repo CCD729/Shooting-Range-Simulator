35656332871162cf43c0b046ad938d36109832a1
ColaDev <funbaba@live.com>
2019-11-19 03:01:49 -0800

    Setting up basic first person camera & movement for test purpose.

---
+++ Shooting Range Simulator/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 mouseCurrent;
+    public float sensitivity = 2.0f;
+
+    GameObject character;
+
+    void Start()
+    {
+        mouseCurrent.x = 90;
+        character = this.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        var mouseGet = new Vector2(Input.GetAxis("Mouse X") * sensitivity, Input.GetAxis("Mouse Y") * sensitivity);
+        mouseCurrent += mouseGet;
+        if (mouseCurrent.y >= 90)
+            mouseCurrent.y = 90;
+        else if (mouseCurrent.y <= -90)
+            mouseCurrent.y = -90;
+        transform.localRotation = Quaternion.AngleAxis(-mouseCurrent.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseCurrent.x, character.transform.up);
+    }
+}
